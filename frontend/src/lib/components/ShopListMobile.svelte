2f30fe47e46e83a7d59113c12ef2938d3dad9333
lucabunino <66553738+lucabunino@users.noreply.github.com>
2026-05-20 16:05:48 +0200

    updatedList

---
+++ frontend/src/lib/components/ShopListMobile.svelte
@@ -0,0 +1,139 @@
+<script>
+    import { onMount } from 'svelte';
+    import { register } from 'swiper/element/bundle';
+    import { m } from '$lib/paraglide/messages.js';
+    import { formatPrice } from '$lib/utils/price';
+    import bp from '$lib/scss/breakpoints.module.scss';
+    import { innerWidth } from 'svelte/reactivity/window';
+
+    let { processedProducts, hoveredProduct = $bindable() } = $props();
+    let swiperEl = $state();
+
+    register();
+
+    $effect(() => {
+        if (innerWidth.current <= parseInt(bp.lg) && swiperEl && processedProducts.length > 0) {
+            const swiperParams = {
+                direction: 'vertical',
+                slidesPerView: 'auto',
+                centeredSlides: false,
+                freeMode: {
+                    enabled: true,
+                    sticky: true,
+                },
+                mousewheel: {
+                    forceToAxis: true,
+                    sensitivity: 1,
+                    thresholdDelta: 10, 
+                },
+                on: {
+                    slideChange: (s) => {
+                        hoveredProduct = processedProducts[s.activeIndex];
+                    }
+                }
+            };
+            Object.assign(swiperEl, swiperParams);
+            swiperEl.initialize();
+        }
+    });
+</script>
+
+<div class="list-mobile">
+    <swiper-container bind:this={swiperEl} init="false">
+        {#each processedProducts as product (product.id)}
+            <swiper-slide>
+                <a 
+                    class="product" 
+                    class:active={hoveredProduct?.id === product.id}
+                    href="/shop/{product.handle}"
+                >
+                    <p class="title wo-36 uppercase {product.availableForSale ? 'availableForSale' : 'soldOut'}">{product.title}</p>
+                    <p class="info in-15 in-13-mb uppercase">
+                        {#if product.collections?.nodes?.length > 0}
+                            <span class="collection">{product.collections.nodes[0].singular?.value || product.collections.nodes[0].title}</span>
+                        {/if}
+                        <span class="price">
+                            {#if product.availableForSale}
+                                {formatPrice(product.priceRange.minVariantPrice.amount, product.priceRange.minVariantPrice.currencyCode)}
+                            {:else}
+                                {m.sold_out()}
+                            {/if}
+                        </span>
+                    </p>
+                </a>
+            </swiper-slide>
+        {/each}
+    </swiper-container>
+</div>
+
+<style lang="scss">
+    @use '$lib/scss/breakpoints.module' as *;
+
+    .list-mobile {
+        display: block;
+        position: relative;
+        height: calc(100vh - 60vh);
+        margin-top: 0;
+        padding: 0 var(--sp-12);
+        background-color: var(--white);
+        z-index: 10;
+
+        &::before, &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            width: 100%;
+            height: 1px;
+            background: var(--black);
+            z-index: 10;
+            pointer-events: none;
+        }
+        &::before { top: 0; }
+        &::after { top: var(--sp-60); }
+
+        swiper-container {
+            height: 100%;
+            
+            swiper-slide {
+                display: flex;
+                align-items: center;
+                color: var(--darkGray);
+                height: var(--sp-60);
+
+                &:global(.swiper-slide-active) {
+                    color: var(--black);
+                }
+                &:global(:last-of-type) {
+                    margin-bottom: calc(100% - var(--sp-60)*3);
+                }
+
+                .product {
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    padding: var(--sp-24) 0;
+                    column-gap: var(--sp-12);
+
+                    .title {
+                        flex-basis: calc(50% - var(--sp-6));
+                    }
+
+                    .info {
+                        display: none;
+                        
+                        .price {
+                            margin-left: auto;
+                        }
+                    }
+
+                    &.active {
+                        .info {
+                            display: contents;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
--- frontend/src/routes/shop/+page.svelte
+++ frontend/src/routes/shop/+page.svelte
@@ -13,13 +13,12 @@
     import { collectionSlugMap } from '$lib/collectionSlugs';
     import { formatPrice } from '$lib/utils/price';
     import bp from '$lib/scss/breakpoints.module.scss';
-    import { register } from 'swiper/element/bundle';
+    import ShopListMobile from '$lib/components/ShopListMobile.svelte';
 
     // stores
     import { getMenu } from '$lib/stores/menu.svelte.js';
 
     // functions
-    register();
     let menuer = getMenu(); menuer.setDark(false); menuer.setSmall(false);
     let { data } = $props();
     const currentLocale = getLocale();
@@ -31,7 +30,6 @@
     let loaded = $state(false);
     let isLoading = $state(false);
     let DURATION = $derived(innerWidth.current > 768 ? 200 : 0);
-    let swiperEl = $state();
     
     let activeCategory = $derived.by(() => {
         const slug = page.url.searchParams.get('category') || 'all';
@@ -70,29 +68,6 @@
         if (activeView === 'list') menuer.setDifference(true);
         else if (activeView === 'grid') menuer.setDifference(false);
         loaded = true;
-    });
-
-    $effect(() => {
-        if (activeView === 'list' && innerWidth.current <= parseInt(bp.md) && swiperEl) {
-            const swiperParams = {
-                direction: 'vertical',
-                slidesPerView: 'auto',
-                centeredSlides: false,
-				freeMode: true,
-                mousewheel: {
-					forceToAxis: true,
-					sensitivity: 1,
-					thresholdDelta: 10, 
-				},
-                on: {
-                    slideChange: (s) => {
-                        hoveredProduct = processedProducts[s.activeIndex];
-                    }
-                }
-            };
-            Object.assign(swiperEl, swiperParams);
-            swiperEl.initialize();
-        }
     });
 
     async function loadMore() {
@@ -267,33 +242,9 @@
 							</a>
 						{/each}
 					</div>
-					<div class="list-mobile">
-						<swiper-container bind:this={swiperEl} init="false">
-							{#each processedProducts as product (product.id)}
-								<swiper-slide>
-									<a 
-										class="product" 
-										class:active={hoveredProduct?.id === product.id}
-										href="/shop/{product.handle}"
-									>
-										<p class="title wo-36 uppercase {product.availableForSale ? 'availableForSale' : 'soldOut'}">{product.title}</p>
-										<p class="info in-15 in-13-mb uppercase">
-											{#if product.collections?.nodes?.length > 0}
-												<span class="collection">{product.collections.nodes[0].singular?.value || product.collections.nodes[0].title}</span>
-											{/if}
-											<span class="price">
-												{#if product.availableForSale}
-													{formatPrice(product.priceRange.minVariantPrice.amount, product.priceRange.minVariantPrice.currencyCode)}
-												{:else}
-													{m.sold_out()}
-												{/if}
-											</span>
-										</p>
-									</a>
-								</swiper-slide>
-							{/each}
-						</swiper-container>
-					</div>
+					{#if innerWidth.current <= parseInt(bp.lg)}
+						<ShopListMobile {processedProducts} bind:hoveredProduct />
+					{/if}
 				{/if}
 			</div>
 		{/key}
@@ -362,7 +313,7 @@ main {
 		height: 100vh;
 	}
 
-	@media (width <= #{$md}) {
+	@media (width <= #{$lg}) {
 		height: auto;
 		width: 100%;
 		height: 60vh;
@@ -490,7 +441,7 @@ main {
 		}
 	}
 
-	@media (width <= #{$md}) {
+	@media (width <= #{$lg}) {
 		&[data-view="list"] {
 			margin-top: unset;
 			justify-content: flex-end;
@@ -585,7 +536,7 @@ main {
                 @media (width <= #{$lg}) {
                     padding: 0 var(--sp-12);
                 }
-                @media (width <= #{$md}) {
+                @media (width <= #{$lg}) {
                     display: none;
                 }
 
@@ -617,78 +568,6 @@ main {
                         }
                         .info {
                             opacity: 1;
-                        }
-                    }
-                }
-            }
-
-            .list-mobile {
-                display: none;
-				
-                @media (width <= #{$md}) {
-                    display: block;
-                    position: relative;
-                    height: calc(100vh - 60vh);
-                    margin-top: 0;
-                    padding: 0 var(--sp-12);
-					background-color: var(--white);
-					z-index: 10;
-
-                    &::before, &::after {
-                        content: '';
-                        position: absolute;
-                        left: 0;
-                        width: 100%;
-                        height: 1px;
-                        background: var(--black);
-                        z-index: 10;
-                        pointer-events: none;
-                    }
-                    &::before { top: 0; }
-                    &::after { top: var(--sp-60); }
-
-                    swiper-container {
-                        height: 100%;
-                        
-                        swiper-slide {
-                            display: flex;
-                            align-items: center;
-                            color: var(--darkGray);
-							height: var(--sp-60);
-
-                            &:global(.swiper-slide-active) {
-                                color: var(--black);
-                            }
-                            &:global(:last-of-type) {
-                                margin-bottom: calc(100% - var(--sp-60)*3);
-                            }
-
-                            .product {
-                                width: 100%;
-                                display: flex;
-                                justify-content: space-between;
-                                align-items: center;
-                                padding: var(--sp-24) 0;
-                                column-gap: var(--sp-12);
-
-                                .title {
-                                    flex-basis: calc(50% - var(--sp-6));
-                                }
-
-                                .info {
-                                    display: none;
-                                    
-                                    .price {
-                                        margin-left: auto;
-                                    }
-                                }
-
-                                &.active {
-                                    .info {
-                                        display: contents;
-                                    }
-                                }
-                            }
                         }
                     }
                 }
